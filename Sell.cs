aca42e6fe4d069161ee3c1a8641888e8f71c4fc5
SvetlanaDubovik <voronina.s.u@gmail.com>
2017-05-02 21:23:41 +0500

    Добавлен обобщенный класс Sell - ячейка в односвязном списке

---
+++ Sell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithList
+{
+    class Sell<T>
+    {
+       public       T Value { get; set; }
+       public Sell<T> Next  { get; set; }
+
+        public Sell(T value)
+        {
+            Value = value;
+        }
+
+    }
+}
