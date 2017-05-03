d49aa6eabfb31b3cd331665c2ac3c09da7f01d9a
SvetlanaDubovik <voronina.s.u@gmail.com>
2017-05-03 06:34:48 +0500

    В Program добавлено тестирование HasCircle в случае, если цикл есть. Прошло успешно

--- Program.cs
+++ Program.cs
@@ -21,6 +21,32 @@ namespace FunWithList
                 Console.WriteLine(str);
 
             Console.WriteLine("\nAnd rabbit with turtle say: {0}", sList1.HasCircle(false));
+            Console.WriteLine("--------------------------------------------------------------");
+
+            SellLinkedList<string> sList2 = new SellLinkedList<string>("~");
+            sList2.AddAtEnd("This");
+            sList2.AddAtEnd("circle");
+            sList2.AddAtEnd("will");
+            sList2.AddAtEnd("have");
+            sList2.AddAtEnd("circle");
+            sList2.AddAtEnd("starting");
+            sList2.AddAtEnd("here,");
+            sList2.AddAtEnd("let's");
+            sList2.AddAtEnd("see");
+            sList2.AddAtEnd("what");
+            sList2.AddAtEnd("rabbit");
+            sList2.AddAtEnd("and");
+            sList2.AddAtEnd("turtle");
+            sList2.AddAtEnd("gonna");
+            sList2.AddAtEnd("say");
+
+            foreach (string str in sList2)
+                Console.WriteLine(str);
+
+            if (sList2.MakeCircle(6) > 0)
+                Console.WriteLine("\nAnd rabbit with turtle say: {0}", sList2.HasCircle(true));
+
+            Console.WriteLine("\nAnd after doing their job they say: {0}", sList2.HasCircle(true));
 
             Console.ReadKey();
         }
