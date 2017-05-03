99d7585942b92091f18c98c3c3b5137c585e84d7
Mikhail Dubovik <m.dubovik@inbox.ru>
2017-05-03 22:07:07 +0500

    Добавлен класс PlanetSystem

---
+++ PlanetSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithList
+{
+    class PlanetSystem
+    {
+        public string StarName { get; private set; }
+
+        List<Planet> planets;
+
+        public PlanetSystem(string starName)
+        {
+            StarName = starName;
+            planets = new List<Planet>();
+        }
+    }
+}
